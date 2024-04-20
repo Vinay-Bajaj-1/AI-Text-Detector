2b47383768823276c605974652530be1689b3701
Vinay Bajaj <vinaybajaj489@gmail.com>
2024-04-20 23:04:52 +0530

    da

--- FrontEnd.py
+++ FrontEnd.py
@@ -18,8 +18,9 @@ def main():
 
     # Input text box for user to enter text
     user_input = st.text_area("Enter the text you want to analyze", "")
-    data = preprocess_text(preprocess_text)
-    print(data)
+    if user_input != "":
+        data = preprocess_text(preprocess_text)
+        print(data)
 
 
 
