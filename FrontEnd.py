4a01d3acf28afd9921ece360d1060b978e4a9f57
Vinay Bajaj <vinaybajaj489@gmail.com>
2024-04-20 20:59:44 +0530

    damnn

--- FrontEnd.py
+++ FrontEnd.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 from sklearn.metrics.pairwise import cosine_similarity
 import joblib
-#from main import preprocess_text
+from main import preprocess_text
 
 # Load the trained model and vectorizer
 model = joblib.load('lr_model.pkl')
@@ -18,8 +18,8 @@ def main():
 
     # Input text box for user to enter text
     user_input = st.text_area("Enter the text you want to analyze", "")
-    #data = preprocess_text(preprocess_text)
-    #print(data)
+    data = preprocess_text(preprocess_text)
+    print(data)
 
 
 
